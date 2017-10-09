90df7fe0de0711e9de5de2162edb7901af5846c5
QIQIQIhome <beycome@163.com>
2017-10-10 00:51:36 +0800

    UI fix
    1. main menu button navigation
    2. scale with screen size map selection canvas

--- DuelForLove/Assets/Code/GlobalManager/MainMenuController.cs
+++ DuelForLove/Assets/Code/GlobalManager/MainMenuController.cs
@@ -1,9 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour 
 {
+	public Button firstSelectedBtn;
+	EventSystem es;
+
+	void Start()
+	{
+		es = FindObjectOfType<EventSystem>();
+		if(es)
+		{
+			es.SetSelectedGameObject(firstSelectedBtn.gameObject);
+		}
+	}
+
 	public void OnClickQuitButton()
 	{
 		Debug.Log("Game quit. Have a good day.");
